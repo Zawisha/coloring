702d9a59c0a2b96c0ef6574473a58a80116d02ef
zawisha <ogniemimechem@gmail.com>
2022-04-29 17:05:55 +0300

    add google analytics

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -36,6 +36,15 @@
     </script>
     <noscript><div><img src="https://mc.yandex.ru/watch/88651975" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
     <!-- /Yandex.Metrika counter -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-1LL2L8VK8T"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-1LL2L8VK8T');
+    </script>
 </head>
 <body class="antialiased" >
 <div id="app">
