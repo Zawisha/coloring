cdf57ab2d33fd1de775d1ff0d5b8bc8669c8f834
zawisha <ogniemimechem@gmail.com>
2022-10-20 12:15:58 +0300

    add sitemap2

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('generate:sitemap')->everyMinute();
+         $schedule->command('generate:sitemap')->daily();
     }
 
     /**
