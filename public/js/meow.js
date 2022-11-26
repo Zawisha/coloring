fe48b83c08f0f107fa4acb541d1ba4cec2a539c8
zawisha <ogniemimechem@gmail.com>
2022-11-26 19:42:32 +0300

    404 new fix3

--- public/js/meow.js
+++ public/js/meow.js
@@ -3,9 +3,9 @@ window.addEventListener("load", myInitFunction);
 // window.onload = function () {
 function myInitFunction()
 {
-    alert(window.innerWidth)
-    if (window.innerWidth < 1210) {
-        alert('asd')
+    // alert(window.innerWidth)
+    if (window.innerWidth < 1200) {
+        // alert('asd')
         let current_width = window.innerWidth
         let offset_finall = Number(-450) + Number(((current_width - 300) / 2))
         document.getElementById("meow_main").style.left = offset_finall + 'px';
