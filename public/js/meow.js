5712768b23abcab7ee78be125f8a2dcf8221fb36
zawisha <ogniemimechem@gmail.com>
2022-11-26 19:23:52 +0300

    404 new fix3

--- public/js/meow.js
+++ public/js/meow.js
@@ -1,10 +1,15 @@
-window.onload = function () {
-    if(window.innerWidth<1200) {
+window.addEventListener("load", myInitFunction);
+
+// window.onload = function () {
+function myInitFunction()
+{
+    if (window.innerWidth < 1200) {
         let current_width = window.innerWidth
         let offset_finall = Number(-450) + Number(((current_width - 300) / 2))
         document.getElementById("meow_main").style.left = offset_finall + 'px';
         document.getElementById("second_meow").style.left = offset_finall + 'px';
 
     }
-    }
+}
+    // }
 
