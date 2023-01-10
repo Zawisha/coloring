2f2bcef40e857dd7c2ef9e19b2e51b89cd1818ac
zawisha <ogniemimechem@gmail.com>
2023-01-10 20:08:34 +0300

    adv in list

--- resources/js/components/FrontColoringListComponent.vue
+++ resources/js/components/FrontColoringListComponent.vue
@@ -24,7 +24,7 @@
                     </div >
                 </div>
                 <div  class="col-12 one_front_coloring " v-for="(colored,index) in coloring_list">
-                    <div class="col-12 row reklama_row_main_list" v-if="(index%reklama_number===0)&&(hide_on_mob)&&(menu_size>991)">
+                    <div class="col-12 row reklama_row_main_list" v-if="(index%reklama_number===0)&&(index!==0)&&(hide_on_mob)&&(menu_size>991)">
                         <div id="yandex_rtb_R-A-1785111-6"></div>
                     </div>
                     <span v-else class="row list_additional_imgs">
